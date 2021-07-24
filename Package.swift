2895e23e632dacb190f93bd6232372221011a20f
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2021-07-24 12:00:49 +0200

    Refactoring

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
     name: "ZenNIO",
     platforms: [
-        .macOS(.v10_14)
+		.macOS(.v10_14)
     ],
     products: [
         .library(name: "ZenNIO", targets: ["ZenNIO"]),
         .library(name: "ZenNIOSSL", targets: ["ZenNIOSSL"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .exact("2.23.0")),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", .exact("2.9.2")),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", .exact("1.15.0")),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", .exact("1.7.0")),
-        .package(url: "https://github.com/apple/swift-log.git", .exact("1.4.0"))
+		.package(url: "https://github.com/apple/swift-nio.git", from: "2.23.0"),
+		.package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.9.2"),
+		.package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.15.0"),
+		.package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
     ],
     targets: [
         .target(
